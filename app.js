873fbe8f0e2ea7b4769f8e798fdbcc822bf929f8
rafalstrozyk <rafal.strozyk.biz@gmail.com>
2020-03-03 12:01:34 +0100

    Add Styling and visual evect

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const ticTacTouController = ( () => {
     const players = ['X', 'O'];
     let player = players[0];
     let win = false;
-
+    let winningboard = [];
     actualPlayer = () => {
         if(player === 'X') {
             player = players[1];
@@ -19,53 +19,38 @@ const ticTacTouController = ( () => {
     winnerChecking = () => {
         let winnerBoards = [
             [
-                board[0],
-                board[1],
-                board[2]
+                0,1,2
             ],
             [
-                board[3],
-                board[4],
-                board[5]
+                3,4,5
             ],
             [
-                board[6],
-                board[7],
-                board[8]
+                6,7,8
             ],
             [
-                board[0],
-                board[3],
-                board[6]
+                0,3,6
             ],
             [
-                board[1],
-                board[4],
-                board[7]
+                1,4,7
             ],
             [
-                board[2],
-                board[5],
-                board[8]
+                2,5,8
             ],
             [
-                board[0],
-                board[4],
-                board[8]
+                0,4,8
             ],
             [
-                board[2],
-                board[4],
-                board[6]
+                2,4,6
             ]
         ];
-        winnerBoards.forEach((el) => {
+        winnerBoards.forEach((el, index) => {
             if(
-                el[0] === player &&
-                el[1] === player &&
-                el[2] === player) {
+                board[el[0]] === player &&
+                board[el[1]] === player &&
+                board[el[2]] === player) {
                 win = true;
                 actualPlayer();
+                winningboard = el;
             }
         });
     }
@@ -97,6 +82,9 @@ const ticTacTouController = ( () => {
         },
         newGame: () => {
             return newGame();
+        },
+        winningBoard: () => {
+            return winningboard;
         }
     };
 
@@ -105,7 +93,8 @@ const ticTacTouController = ( () => {
 const UIcontroller = ( (TTTCtrl) => {
 
     const DOMclassesAndId = {
-        resetButon: 'btn-reset'
+        resetButon: 'btn-reset',
+        whichPlayer: 'which-player'
     };
 
     const DOMBoardArray = ['one', 'two', 'three',
@@ -119,13 +108,26 @@ const UIcontroller = ( (TTTCtrl) => {
     }
 
     winnerUI = (player) => {
+        let winnerBoard = TTTCtrl.winningBoard();
+        winnerBoard.forEach((el) => {
+            document.getElementById(DOMBoardArray[el]).classList.remove('selected');
+            document.getElementById(DOMBoardArray[el]).classList.add('winner-board');
+
+        });
+        document.getElementById('winner').style.display = 'block';
         document.getElementById('winner').textContent = 'Winner: ' + player;
     }
 
     newGameUI = () => {
         DOMBoardArray.forEach((domElem) => {
             document.getElementById(domElem).textContent = '';
-            document.getElementById('winner').textContent = 'Winner: '
+            document.getElementById('winner').style.display = 'none';
+            document.getElementById(DOMclassesAndId.whichPlayer).textContent = 'X';
+            document.getElementById(DOMclassesAndId.whichPlayer).classList.remove('right');
+            document.getElementById(DOMclassesAndId.whichPlayer).classList.add('lef');
+            document.getElementById(domElem).classList.add('unselected');
+            document.getElementById(domElem).classList.remove('winner-board');
+            document.getElementById(domElem).classList.remove('selected');
         });
     };
 
@@ -158,6 +160,8 @@ const controller = ( (UICtrl,TTTCtrl) => {
         document.getElementById(domElem).addEventListener('click', () => {
             let win = TTTCtrl.win();
             if(!win) {
+                document.getElementById(domElem).classList.remove('unselected');
+                document.getElementById(domElem).classList.add('selected');
                 UICtrl.boardMovement(domElem);
                 TTTCtrl.boardUpdate(index);
             } 
@@ -170,6 +174,9 @@ const controller = ( (UICtrl,TTTCtrl) => {
             let player = TTTCtrl.player();
             let win = TTTCtrl.win();
             if(!win) {
+                document.getElementById(DOMclassesAndId.whichPlayer).textContent = player;
+                document.getElementById(DOMclassesAndId.whichPlayer).classList.toggle('lef');
+                document.getElementById(DOMclassesAndId.whichPlayer).classList.toggle('right');
                 winerController;
             } else {
                 UICtrl.winnerUI(player);
